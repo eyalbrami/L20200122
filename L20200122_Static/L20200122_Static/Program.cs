e60d12a358a8978c7aabff14a79fb0d002a7e0c4
Eyal Brami <eyalbrami@gmail.com>
2020-01-22 20:11:29 +0200

    Initial commit

---
+++ L20200122_Static/L20200122_Static/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20200122_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Car details :");
+            Car.Counter = 0;
+            Car car = new Car();
+            while (Car.SetCounter())
+            {
+                Console.WriteLine("Please enter Car model :");
+                car.SetModel(Console.ReadLine());
+                Console.WriteLine("Please enter Car speed :");
+
+                car.SetCurrentSpeed(int.Parse(Console.ReadLine()));
+                Car.Counter += 1;
+
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        //Targil 1 : avoid created more than 10 objects of the same class.
+
+
+        public class Car
+        {
+            public string PlateNumber;
+            public string Color;
+            public string Manufacturer;
+            public bool EBS;
+            public static int Counter { get; set; } 
+
+
+            int currentSpeed;
+            string Model;
+
+            //public Car() : this(10)
+            //{
+
+            //}
+            //public Car(int initialSpeed) : this(initialSpeed, "TT") { }
+
+
+            public Car(int initialSpeed = 10, string model = "TT")
+            {
+                SetModel(model);
+                SetCurrentSpeed(initialSpeed);
+                SetCounter();
+            }
+            public static bool SetCounter() {
+                if (Counter >= 10)
+                {
+                    Console.WriteLine($"You Exceeded the creation of { Counter } Objects");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine($"You need to choose another { 10 - Counter }");
+                    return true;
+                }
+            }
+
+            public void PrintCar()
+            {
+                //Console.WriteLine(
+                //    "PlateNumber : {0}, Color: {1}, Speedometer : {2}, Manufacturer: {3}, Model: {4}, EBS Exists ? {5}", PlateNumber, Color, currentSpeed, Manufacturer, Model, EBS
+                //    );
+
+                Console.WriteLine("Model: {0}, Speedometer : {1}", Model, currentSpeed);
+
+            }
+            public void SpeedUp(int delta)
+            {
+                currentSpeed += delta;
+            }
+
+            //Setter
+            public void SetModel(string m)
+            {
+                this.Model = m; //this is the pointer to the current object created/
+            }
+            public void SetCurrentSpeed(int s)
+            {
+                if (s <= 0)
+                    return;
+                currentSpeed = s;
+            }
+            //Getter
+            public string GetModel()
+            {
+                return Model;
+            }
+            public int GetCurrentSpeed()
+            {
+                return currentSpeed;
+            }
+
+        }
+
+    }
+}
---
+++ Targil_1/Targil_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil_1
+{
+    class Program
+    {
+
+        /*
+        ברגע שיש התייחסות לשדה סטטי כלומר בפעם הראשונה שיש התייחסות לשדה סטטי
+        מתרחש אתחול לכל השדות הסטטיים.
+
+         * */
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(MyClass.StaticField);
+            MyClass.Foo();
+            Console.WriteLine("First...");
+
+            for (int i = 0; i < 11; i++)
+            {
+                new MyClass(i);
+                
+            }
+            Console.ReadLine();
+
+        }
+        class MyClass
+        {
+            static int Counter = InitialValue();
+            public static int StaticField = 1;
+            public MyClass(int x)
+            {
+                //if(counter >=10)
+                //{
+                //    Console.WriteLine("Too Many objects");
+                //}
+                Console.WriteLine(x);
+            }
+            public static int InitialValue() {
+                Console.WriteLine("initial Value");
+                return 0;
+            }
+            public static void Foo() {
+              //  Console.WriteLine("Foo");
+            }
+        }
+    }
+}
